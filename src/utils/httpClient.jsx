0823f3cdc332ad28717a2418cbfe83a60d723cdb
Albatroz2 <141192201+Albatroz2@users.noreply.github.com>
2024-07-29 00:13:10 -0500

    Update httpClient.jsx

--- src/utils/httpClient.jsx
+++ src/utils/httpClient.jsx
@@ -4,8 +4,7 @@ export function get(path) {
     return fetch(API + path, {
         headers: {
             Authorization:
-                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNmE0NTY0NjM0ODRhYTM2YjVhODgxMWRhZDhmOGIwOSIsIm5iZiI6MTcyMjIyNTcyMC4zNzQ1NzUsInN1YiI6IjY1NDQ4ZmE5OWNjNjdiMDEzYTMwMWE1NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Za--dthkpDsiPkgl5UUKwJXUD1_ogZeZArotIt1v-o",
-            "Content-Type": "application/json;charset=utf-8",
+                "Bearer,"
             },
     }).then((result) => result.json());
 }
